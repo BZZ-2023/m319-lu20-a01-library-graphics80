5df9c6ecb7f72d79b530e88896b074a63aab452c
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-01-18 12:41:32 +0100

    asdf

--- library.py
+++ library.py
@@ -1,25 +1,102 @@
+from datetime import datetime
+from math import inf
+from rental import Rental
+
+
 def init_books():
-    pass
+    """
+    inits the books dictionary
+    :return:
+    """
+    books = {
+        'LOTR 1': [],
+        'LOTR 2': [],
+        'LOTR 3': []
+    }
+    return books
 
 
 def read_rental():
-    pass
+    """
+    gets the rental details from customer
+    :return:
+    """
+    start_date = read_date('Enter rental date (dd.mm.yyyy): ')
+    num_days = read_int('Enter number of rental days: ')
+    end_date = read_date('Enter return date (dd.mm.yyyy): ')
+    rental = Rental(start_date,end_date,num_days)
+    return rental
 
 
 def add_rental(books):
-    print('Here is the book rental part')
+    """
+    adds a new rental
+    :param books:
+    :return:
+    """
+    while True:
+        book_name = input('Enter the book')
+
+        if book_name in books:
+            rental = read_rental()
+            books[book_name].append(rental)
+            choice = input('Do you want to enter another booking (y/n)')
+            if choice != 'y':
+                break
+        else:
+            print('Book not found')
 
 
 def show_balance(books):
-    print('Print the book balance here')
+    """
+    Prints the balance
+    :param books:
+    :return:
+    """
+    for book, rentals in books.items():
+        print(f'Statement for {book}')
+
+        total = 0
+        for rental in rentals:
+            #print(f'  - {datetime.strftime(rental.rental_date,"%d.%m.%Y")}: CHF {rental.cost}')
+            print(f'  - {rental.rental_date.strftime("%d.%m.%Y")}: CHF {rental.cost}')
+            total += rental.cost
+        print(f'Total: CHF {total}')
 
 
-def read_int(prompt, minimum, maximum):
-    return int(input(prompt))  # Placeholder to let main run. Delete when doing the aassignment
-
+def read_int(text_to_display, lower_bound = float(-inf), upper_bound = float(inf)):
+    '''
+    Read an int from the user within bounds
+    '''
+    while True:
+        try:
+            num = int(input(text_to_display))
+        except ValueError:
+            print("Please, enter a whole number!")
+            continue
+        else:
+            if num < lower_bound:
+                print("Please, enter a number greater than or equal to", lower_bound)
+                continue
+            if num > upper_bound:
+                print("Please, enter a number less than or equal to", upper_bound)
+                continue
+            return num
 
 def read_date(prompt):
-    return None
+    """
+    Reads a date from console format dd.mm.yyyy
+    :param prompt: Text to show the user
+    :return:
+    """
+    while True:
+        s = input(prompt)
+        try:
+            dt = datetime.strptime(s, '%d.%m.%Y')
+            return dt
+        except ValueError:
+            print("Please enter a valid date.")
+
 
 
 def main():
--- rental.py
+++ rental.py
@@ -1,2 +1,21 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+@dataclass
 class Rental:
-    pass
+    rental_date: datetime
+    return_date: datetime
+    num_rental_days: int
+
+    @property
+    def cost(self):
+        """
+        Caculate the cost of the rental
+        :return: rentalcost
+        """
+        base_cost = 4.50
+        overdue_days = (self.return_date - self.rental_date).days - self.num_rental_days
+        if overdue_days < 0:
+            overdue_days == 0
+        penalty = 3.35 * overdue_days
+        return base_cost + penalty
